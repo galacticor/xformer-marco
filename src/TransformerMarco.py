4285820c12bf6a45ab4685d93e88d8b404248bf9
Widyanto Nugroho <widyantohadi@gmail.com>
2022-08-21 23:15:17 +0700

    add docker config

--- src/TransformerMarco.py
+++ src/TransformerMarco.py
@@ -11,7 +11,7 @@ import torch
 import torch.nn.functional as F
 
 from MarcoDataset import MarcoDataset
-from .specs import ArgParams
+from specs import ArgParams
 
 
 class TransformerMarco(pl.LightningModule):
