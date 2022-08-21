cdfbfcc77db9aaba39955a5a4e13959b033e5f50
Widyanto Nugroho <widyantohadi@gmail.com>
2022-08-21 22:49:16 +0700

    set for cpu

--- src/run_longformer_marco.py
+++ src/run_longformer_marco.py
@@ -1,4 +1,6 @@
+import dataclasses
 import pytorch_lightning as pl
+import torch
 
 import argparse
 from TransformerMarco import TransformerMarco
@@ -9,6 +11,8 @@ import os
 
 from pytorch_lightning import seed_everything
 
+from specs import ArgParams
+
 seed_everything(42)
 
 
@@ -18,7 +22,6 @@ def main(hparams):
     loggers = []
     if hparams.use_wandb:
         wandb_logger = WandbLogger(
-            project="xformer-marco",
             entity="galacticor",
             name=f"Albert-passage-{hparams.slurm_job_id}",
         )
@@ -31,21 +34,19 @@ def main(hparams):
         loggers.append(tb_logger)
 
     checkpoint_callback = ModelCheckpoint(
-        filepath=os.path.join(os.getcwd(), "checkpoints"),
         save_top_k=3,
         verbose=True,
         monitor="val_epoch_loss",
         mode="min",
-        prefix="",
     )
 
     # This Trainer handles most of the stuff.
     # Enables distributed training with one line:
     # https://towardsdatascience.com/trivial-multi-node-training-with-pytorch-lightning-ff75dfb809bd
     trainer = pl.Trainer(
-        gpus=hparams.gpus,
+        devices=hparams.device,
         num_nodes=hparams.num_nodes,
-        distributed_backend=hparams.distributed_backend,
+#         distributed_backend=hparams.distributed_backend,
         # control the effective batch size with this param
         accumulate_grad_batches=hparams.trainer_batch_size,
         # Training will stop if max_steps or max_epochs have reached (earliest).
@@ -66,97 +67,119 @@ def main(hparams):
 
 if __name__ == "__main__":
 
-    print(os.environ["SLURM_NODELIST"])
-    parser = argparse.ArgumentParser(description="Transformer-MARCO")
-    # MODEL SPECIFIC
-    parser.add_argument(
-        "--max_seq_len",
-        type=int,
-        default=4096,
-        help="Maximum number of wordpieces of the sequence",
-    )
-    parser.add_argument("--lr", type=float, default=3e-5, help="Learning rate")
-    parser.add_argument("--num_warmup_steps", type=int, default=2500)
-    parser.add_argument("--num_training_steps", type=int, default=120000)
-    parser.add_argument(
-        "--val_check_interval",
-        type=int,
-        default=20000,
-        help="Run through dev set every N steps",
-    )
-    parser.add_argument(
-        "--clf_dropout",
-        type=float,
-        default=-1.0,
-        help="Dropout for classifier. Set negative to use transformer dropout",
-    )
-    parser.add_argument(
-        "--num_workers", type=int, default=0, help="Num subprocesses for DataLoader"
+    # print(os.environ["SLURM_NODELIST"])
+    # parser = argparse.ArgumentParser(description="Transformer-MARCO")
+    # # MODEL SPECIFIC
+    # parser.add_argument(
+    #     "--max_seq_len",
+    #     type=int,
+    #     default=4096,
+    #     help="Maximum number of wordpieces of the sequence",
+    # )
+    # parser.add_argument("--lr", type=float, default=3e-5, help="Learning rate")
+    # parser.add_argument("--num_warmup_steps", type=int, default=2500)
+    # parser.add_argument("--num_training_steps", type=int, default=120000)
+    # parser.add_argument(
+    #     "--val_check_interval",
+    #     type=int,
+    #     default=20000,
+    #     help="Run through dev set every N steps",
+    # )
+    # parser.add_argument(
+    #     "--clf_dropout",
+    #     type=float,
+    #     default=-1.0,
+    #     help="Dropout for classifier. Set negative to use transformer dropout",
+    # )
+    # parser.add_argument(
+    #     "--num_workers", type=int, default=0, help="Num subprocesses for DataLoader"
+    # )
+
+    # # EXPERIMENT SPECIFIC
+    # parser.add_argument(
+    #     "--data_dir",
+    #     type=str,
+    #     default="../data",
+    # )
+
+    # # effective batch size will be:
+    # # trainer_batch_size * data_loader_bs
+    # parser.add_argument(
+    #     "--trainer_batch_size",
+    #     type=int,
+    #     default=5,
+    #     help="Batch size for Trainer. Accumulates grads every k batches",
+    # )
+    # parser.add_argument(
+    #     "--data_loader_bs", type=int, default=1, help="Batch size for DataLoader object"
+    # )
+    # parser.add_argument(
+    #     "--val_data_loader_bs",
+    #     type=int,
+    #     default=0,
+    #     help="Batch size for validation data loader. If not specified,\
+    #                     --data_loader_bs is used.",
+    # )
+    # parser.add_argument(
+    #     "--use_10_percent_of_dev",
+    #     type=int,
+    #     default=1,
+    #     help="0 to use the full dev dataset, else to use 10%% only",
+    # )
+    # parser.add_argument("--epochs", type=int, default=5)
+    # parser.add_argument("--slurm_job_id", type=int, default=1)
+    # parser.add_argument(
+    #     "--use_wandb", type=int, default=0, help="Use Weights&Biases (wandb) logger"
+    # )
+    # parser.add_argument(
+    #     "--use_tensorboard",
+    #     type=int,
+    #     default=1,
+    #     help="Use TensorBoard logger (default in PL)",
+    # )
+
+    # # Distributed training
+    # parser.add_argument("--gpus", type=int, default=1, help="Num of GPUs per node")
+    # parser.add_argument(
+    #     "--num_nodes", type=int, default=1, help="Num nodes allocated by SLURM"
+    # )
+    # parser.add_argument(
+    #     "--distributed_backend",
+    #     type=str,
+    #     default="dp",
+    #     help="Use distributed backend: dp/ddp/ddp2",
+    # )
+    # parser.add_argument(
+    #     "--model_name",
+    #     type=str,
+    #     default="allenai/longformer-base-4096",
+    #     help="Full name of: bert|albert|longformer",
+    # )
+
+    hparams = ArgParams(
+        run_name="test-1",
+        model_name="allenai/longformer-base-4096",
+        learning_rate=3e-5,
+        num_warmup_steps=1000,
+        num_training_steps=120000,
+        data_dir="../input/msmarco-document",
+        max_seq_len=1536,
+        data_loader_bs=4,
+        val_data_loader_bs=2,
+        num_workers=1,
+        trainer_batch_size=1,
+        epochs=1,
+        use_wandb=False,
+        use_tensorboard=False,
+        device=0 if torch.cuda.is_available() else 1,
+    #     gpus=1,
+        num_nodes=1,
     )
 
-    # EXPERIMENT SPECIFIC
-    parser.add_argument(
-        "--data_dir",
-        type=str,
-        default="../data",
-    )
-
-    # effective batch size will be:
-    # trainer_batch_size * data_loader_bs
-    parser.add_argument(
-        "--trainer_batch_size",
-        type=int,
-        default=5,
-        help="Batch size for Trainer. Accumulates grads every k batches",
-    )
-    parser.add_argument(
-        "--data_loader_bs", type=int, default=1, help="Batch size for DataLoader object"
-    )
-    parser.add_argument(
-        "--val_data_loader_bs",
-        type=int,
-        default=0,
-        help="Batch size for validation data loader. If not specified,\
-                        --data_loader_bs is used.",
-    )
-    parser.add_argument(
-        "--use_10_percent_of_dev",
-        type=int,
-        default=1,
-        help="0 to use the full dev dataset, else to use 10%% only",
-    )
-    parser.add_argument("--epochs", type=int, default=5)
-    parser.add_argument("--slurm_job_id", type=int, default=1)
-    parser.add_argument(
-        "--use_wandb", type=int, default=0, help="Use Weights&Biases (wandb) logger"
-    )
-    parser.add_argument(
-        "--use_tensorboard",
-        type=int,
-        default=1,
-        help="Use TensorBoard logger (default in PL)",
-    )
-
-    # Distributed training
-    parser.add_argument("--gpus", type=int, default=1, help="Num of GPUs per node")
-    parser.add_argument(
-        "--num_nodes", type=int, default=1, help="Num nodes allocated by SLURM"
-    )
-    parser.add_argument(
-        "--distributed_backend",
-        type=str,
-        default="dp",
-        help="Use distributed backend: dp/ddp/ddp2",
-    )
-    parser.add_argument(
-        "--model_name",
-        type=str,
-        default="allenai/longformer-base-4096",
-        help="Full name of: bert|albert|longformer",
-    )
-
-    hparams = parser.parse_args()
+    # hparams = parser.parse_args()
     if hparams.val_data_loader_bs <= 0:
         hparams.val_data_loader_bs = hparams.data_loader_bs
+
+    hparams = argparse.Namespace(**dataclasses.asdict(hparams))
     print(hparams)
-    # main(hparams)
+    main(hparams)
--- src/specs.py
+++ src/specs.py
@@ -11,12 +11,16 @@ class ArgParams:
     val_data_loader_bs: int
     num_workers: int
     trainer_batch_size: int
+    device: int = -1
+    use_10_percent_of_dev: bool = False
     epochs: int = 2
     learning_rate: float = 3e-5
     num_warmup_steps: int = 2500
     num_training_steps: int = 120000
+    val_check_interval: float = 0.25
     use_wandb: bool = True
     use_tensorboard: bool = True
     gpus: int = 1
     num_nodes: int = 1
     distributed_backend: str = "dp"
+    slurm_job_id: str = "ssss"
