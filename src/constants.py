44748986680cdabbc388500315dea73ca84065d2
Widyanto Nugroho <widyantohadi@gmail.com>
2022-08-21 23:19:02 +0700

    add docker config

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
 import os
 
-DATA_DIR = os.getenv("DATA_DIR", "../input/msmarco-document")
-DOCS_FILE = os.getenv("DOCS_FILE", "../input/msmarco-docs/docs.tsv")
+DATA_DIR = os.getenv("DATA_DIR", "../input")
+DOCS_FILE = os.getenv("DOCS_FILE", "../input/docs.tsv")
