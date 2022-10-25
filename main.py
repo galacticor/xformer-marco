cdc1e5d101a5bb3f74252df3ab9a0a269181fc69
Widyanto Nugroho <widyantohadi@gmail.com>
2022-10-25 07:40:57 +0700

    add inline params

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from src.constants import INPUT_DIR
 from src.specs import ArgParams
 
 
+def validation(params: ArgParams) -> ArgParams:
+    params.device = [int(x) for x in params.device]
+
+    return params
+
+
 if __name__ == "__main__":
     args = sys.argv
 
@@ -43,17 +49,39 @@ if __name__ == "__main__":
         num_nodes=1
     )
 
-    if args[1] == "bert":
+    parser = argparse.ArgumentParser(description='Transformer-MARCO')
+    
+    parser.add_argument("--model_name", type=str, default="bert",
+                        help="Model name")
+
+    parser.add_argument("--data_loader_bs", type=int, default=8,
+                        help="Training batch size")
+
+    parser.add_argument("--val_data_loader_bs", type=int, default=4,
+                        help="Validation batch size")
+
+    parser.add_argument("--num_workers", type=int, default=12,
+                        help="Num subprocesses for DataLoader")
+
+    parser.add_argument("--device", type=list, default=1,
+                        help="GPU Device ID")
+
+    parser.add_argument("--ckpt_path", type=str, default=None,
+                        help="Checkpoint Path")
+
+    param_from_parser = parser.parse_args()
+
+    if param_from_parser.model_name == "bert":
         runner = longformer.main
         hparams.model_name = bert_model_name
         hparams.max_seq_len = 512
 
-    if args[1] == "longformer":
+    if param_from_parser.model_name == "longformer":
         runner = longformer.main
         hparams.model_name = longformer_model_name
         hparams.max_seq_len = 4096
 
-    if args[1] == "nystromformer":
+    if param_from_parser.model_name == "nystromformer":
         runner = longformer.main
         hparams.model_name = nystromformer_model_name
         hparams.max_seq_len = 4096
@@ -62,9 +90,17 @@ if __name__ == "__main__":
     if hparams.val_data_loader_bs <= 0:
         hparams.val_data_loader_bs = hparams.data_loader_bs
 
+    hparams.data_loader_bs = param_from_parser.data_loader_bs
+    hparams.val_data_loader_bs = param_from_parser.val_data_loader_bs
+    hparams.num_workers = param_from_parser.num_workers
+    hparams.device = param_from_parser.device
+    hparams.ckpt_path = param_from_parser.ckpt_path
+
+    hparams = validation(hparams)
+
     hparams = argparse.Namespace(**dataclasses.asdict(hparams))
     print(hparams)
-    trainer, model = runner(hparams)
+    # trainer, model = runner(hparams)
 
-    trainer.fit(model)
-    trainer.save_checkpoint(f"{hparams.model_name}_{hparams.run_name}_sz{hparams.training}:{hparams.validation}_bs{hparams.data_loader_bs}:{hparams.val_data_loader_bs}_last.ckpt")
+    # trainer.fit(model)
+    # trainer.save_checkpoint(f"{hparams.model_name}_{hparams.run_name}_sz{hparams.training}:{hparams.validation}_bs{hparams.data_loader_bs}:{hparams.val_data_loader_bs}_last.ckpt")
